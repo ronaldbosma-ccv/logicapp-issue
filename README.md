78dbacfb3125f18fb304dab33ad943f25922236e
Ronald Bosma <ronaldbosma@ccv.eu>
2025-07-14 15:26:41 +0200

    Add sample logic app workflow with test

---
+++ README.md
@@ -0,0 +1,6 @@
+Execute test locally:
+1. Run Azurite (on default ports)
+1. Open [MyLogicAppTests.sln](/tests/MyLogicAppTests/MyLogicAppTests.sln)
+1. Restore NuGet packages
+1. Build solution
+1. Execute the test
---
+++ tests/MyLogicAppTests/MyWorkflowTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using LogicAppUnit;
+
+namespace MyLogicAppTests
+{
+    [TestClass]
+    public sealed class MyWorkflowTests : WorkflowTestBase
+    {
+        [TestMethod]
+        public void MyWorkflow_Should_Return_200_OK()
+        {
+            // Arrange
+            Initialize("../../../../../src/MyWorkspace/MyLogicApp", "MyWorkflow");
+            using var testRunner = CreateTestRunner();
+
+            // Act
+            var result = testRunner.TriggerWorkflow(HttpMethod.Post);
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
+            Assert.AreEqual(WorkflowRunStatus.Succeeded, testRunner.WorkflowRunStatus);
+        }
+    }
+}
